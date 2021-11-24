36b6fffb9a07a2175e57f0f1679dba7864d92658
Romain Blatrix <romain.blatrix@gmail.com>
2021-11-24 13:51:44 +0100

    update Readme schema

--- mmp/src/App.js
+++ mmp/src/App.js
@@ -3,14 +3,6 @@ import React from "react";
 const App = () => (
   <div>
     <h1>MMP</h1>
-    {/* {Object.entries(routes).map(([domain, routes]) => (
-      <div>
-        <h2>{domain}</h2>
-        <ul>
-          {routes.map(({path, label}) => <li>{label}</li>)}
-        </ul>
-      </div>
-    ))} */}
   </div>
 );
 
